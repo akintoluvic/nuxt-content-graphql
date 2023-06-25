0855c73976929dfcdf1babfa9dab608e3ebbfe42
Victor Akintunde <akintoluvic@gmail.com>
2023-06-25 13:53:53 +0100

    pages and routing

--- pages/uses.vue
+++ pages/uses.vue
@@ -1,7 +1,7 @@
 <script setup></script>
 <template>
 <section class="text-slate-400">
-    <h1>🌟 I'm Boba Yoga</h1>
+    <h1>🚀 Hey Boba Uses</h1>
     <p>My favorite boba is taro milk tea with boba.</p>
 </section>
 </template>
